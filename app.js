b1200d2e115148ac018ed074f84da818f2eca365
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2023-07-13 13:29:56 +0900

    230713
    프로젝트 초기 세팅

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+//포트번호 설정
+
+const PORT = 3000;
+
+//정적 파일 접근 및 렌더링 설정
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/src'));
+app.set('views', __dirname + '/src/public/views');
+app.engine('html', require('ejs').renderFile);  
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+});
+
+
+app.listen(PORT, ()=>{
+    console.log('Runing the app!')
+})
